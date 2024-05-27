8402a998060a51f8ada509af79ca6bc628e73998
rmri <intonarumori@gmail.com>
2024-05-27 18:31:44 +0200

    added bytereader

--- lib/byte_utilities.dart
+++ lib/byte_utilities.dart
@@ -1,4 +1,5 @@
 library byte_utilities;
 
 export 'src/byte_logging.dart';
+export 'src/byte_reader.dart';
 export 'src/checksum.dart';
---
+++ lib/src/byte_reader.dart
@@ -0,0 +1,25 @@
+class ByteReader {
+  final List<int> _bytes;
+  int _position = 0;
+
+  ByteReader(List<int> bytes) : _bytes = bytes;
+
+  void skip(int count) {
+    _position += count;
+  }
+
+  int readByte() {
+    final value = _bytes[_position];
+    _position += 1;
+    return value;
+  }
+}
+
+extension AkaiByteReader on ByteReader {
+  int readSW() {
+    // Based on: AkaiS900SysExSpec.pdf -> SW type
+    // K-excess offset binary: K = 2^(12-1)
+    // https://en.wikipedia.org/wiki/Offset_binary
+    return ((readByte() & 0x7F) << 5 | (readByte() >> 2) & 0x1F) - 0x800;
+  }
+}
