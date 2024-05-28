ee5efb33a31ebbae58270d04590353439e81b405
rmri <intonarumori@gmail.com>
2024-05-28 10:34:14 +0200

    updates

--- lib/src/byte_writer.dart
+++ lib/src/byte_writer.dart
@@ -1,10 +1,14 @@
 import 'dart:typed_data';
 
 class ByteWriter {
-  //
+  
   final _bytes = BytesBuilder();
 
   void writeByte(int byte) {
     _bytes.addByte(byte);
+  }
+
+  List<int> toBytes() {
+    return _bytes.toBytes();
   }
 }
