685d73dacf5efffb67563f87522e9b0845dcd293
rmri <intonarumori@gmail.com>
2025-02-25 11:43:02 +0100

    added int8 writing

--- lib/src/byte_writer.dart
+++ lib/src/byte_writer.dart
@@ -14,6 +14,7 @@ class ByteWriter {
   void writeFloat32(double x) => _writeBytes(_fbuf..setFloat32(0, x, _endian), 4);
   void writeFloat64(double x) => _writeBytes(_fbuf..setFloat64(0, x, _endian), 8);
 
+  void writeInt8(int x) => _writeBytes(_fbuf..setInt8(0, x), 1);
   void writeUint8(int x) => _writeBytes(_fbuf..setUint8(0, x), 1);
   void writeUint16(int x) => _writeBytes(_fbuf..setUint16(0, x, _endian), 2);
   void writeUint32(int x) => _writeBytes(_fbuf..setUint32(0, x, _endian), 4);
